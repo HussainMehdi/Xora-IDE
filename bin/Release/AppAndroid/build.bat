b41587bda02f599759d376a4182d84a4b5e80b7c
Syed Hussain Mehdi <k122313@nu.edu.pk>
2016-04-12 19:39:57 +0500

    Updated and sorted ith SLN

--- bin/Release/AppAndroid/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set ANDROID_HOME=D:\Android\android-sdk
-set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
-call cordova build -d android
-explorer platforms\android\build\outputs\apk
-
-
